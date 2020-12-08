85b9287addf0e2045027b81df34b0351b590dfff
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2020-12-07 20:49:25 -0800

    Spawn Blocks

---
+++ Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject blockPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++) {
+            if (randomIndex == i) continue;
+            Instantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity); 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
