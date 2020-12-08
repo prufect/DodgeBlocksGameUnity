bd52af6c05774809e80e4d3ddaf387dbfff88976
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2020-12-07 20:33:35 -0800

    Add Player and Block with Physics

---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 15f;
+    public float mapWidth = 10f;
+
+    private Rigidbody2D rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate() {
+        float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+
+        Vector2 newPos = rb.position + Vector2.right * x;
+        newPos.x = Mathf.Clamp(newPos.x, -mapWidth, mapWidth);
+        
+        rb.MovePosition(newPos);
+    }
+}
