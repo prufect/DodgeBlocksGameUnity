b933540105ebdf5c898c6cd3723203d4ef0379a1
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2020-12-07 20:57:07 -0800

    Repeatedly Spawn blocks in waves

--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -6,21 +6,31 @@ public class BlockSpawner : MonoBehaviour
 {
     public Transform[] spawnPoints;
     public GameObject blockPrefab;
+    public float timeBetweenWaves = 1f;
+
+    private float timeToSpawn = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= timeToSpawn) {
+            spawnBlocks();
+            timeToSpawn = Time.time + timeBetweenWaves;
+        }
+    }
+
+    void spawnBlocks() {
         int randomIndex = Random.Range(0, spawnPoints.Length);
 
         for (int i = 0; i < spawnPoints.Length; i++) {
             if (randomIndex == i) continue;
             Instantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity); 
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
