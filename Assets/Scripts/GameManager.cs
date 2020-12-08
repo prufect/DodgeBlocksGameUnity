cc48982a1125ca5b10d4637dd7e82aad55323fa5
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2020-12-07 21:14:56 -0800

    Add slow down effect

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public float slowDownFactor = 10f;
+
     public void endGame() {
+        StartCoroutine(RestartLevel());
+    }
+
+    IEnumerator RestartLevel() {
+        Time.timeScale = 1f/slowDownFactor;
+        Time.fixedDeltaTime = Time.fixedDeltaTime/slowDownFactor;
+
+        yield return new WaitForSeconds(1f / slowDownFactor);
+
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = Time.fixedDeltaTime * slowDownFactor;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
